30e4ea3e7338f825454e135bcb0870b40ad03a9c
nautili <lear.vg@gmail.com>
2015-04-03 14:40:48 -0500

    Random walk added

--- rb.py
+++ rb.py
@@ -3,6 +3,7 @@
 
 import pygame
 import random
+import rbutils
 
 class SceneBase:
      def __init__(self):
@@ -63,41 +64,76 @@ def run_game(width, height, fps, starting_scene):
 
 
 class Robot:
-     def __init__(self, x, y):
+     def __init__(self, rid, x, y, maxSpeed = 0.005, vis = 0.15):
+          self.rid = rid
           self.x = x
           self.y = y
-          self.nn = []
+          self.vis = vis
+          self.maxSpeed = maxSpeed 
+          self.nearSame = []
+          self.nearOther = []
 
 class RobotScene(SceneBase):
-     def __init__(self, numRed, numBlue):
+     def __init__(self, numRed, numBlue, blueVision):
           SceneBase.__init__(self)
+          
+          self.showBlue = False
+          self.showRed = False
           self.redRobots = []
           for i in range(numRed):
-               self.redRobots += [Robot(random.random(), random.random())]
+               self.redRobots += [Robot(i, random.random(), random.random())]
           self.blueRobots = []
           for i in range(numBlue):
-               self.blueRobots += [Robot(random.random(), random.random())]
+               self.blueRobots += [Robot(i, random.random(), random.random())]
           
     
      def ProcessInput(self, events, pressed_keys):
-          pass
+          for event in events:
+               if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+                    self.showRed = not self.showRed
+               if event.type == pygame.KEYDOWN and event.key == pygame.K_b:
+                    self.showBlue = not self.showBlue
         
      def Update(self):
-          pass
+          for blue in self.blueRobots:
+               blue.nearSame = []
+               for other in self.blueRobots:
+                    if other != blue and rbutils.dist(blue, other) < blue.vis:
+                         blue.nearSame += [other]
+          for blue in self.blueRobots:
+               blue.nearOther = []
+               for other in self.redRobots:
+                    if rbutils.dist(blue, other) < blue.vis:
+                         blue.nearOther += [other]
+          rbutils.randomStep(self.redRobots, 0, 1, 0, 1)
     
      def Render(self, screen):
-          screen.fill((255, 255, 255))
-          radius = 4
           red = (215, 40, 60)
           aqua = (0, 140, 255)
+          egg = (235, 235, 211)
+          black = (0, 0, 0)
+          screen.fill(egg)
+          radius = 4
+          lineWidth = 1
 
           for rr in self.redRobots:
                newx = int(rr.x * screen.get_width())
-               newy = int(rr.y * screen.get_height())
+               newy = int(rr.y * screen.get_height())               
                pygame.draw.circle(screen, red, (newx, newy), radius)
           for br in self.blueRobots:
                newx = int(br.x * screen.get_width())
                newy = int(br.y * screen.get_height())
+               
+               if self.showBlue:
+                    for nn in br.nearSame:
+                         ox = int(nn.x * screen.get_width())
+                         oy = int(nn.y * screen.get_height())
+                         pygame.draw.line(screen, black, (newx, newy), (ox, oy), lineWidth)
+               if self.showRed:
+                    for nn in br.nearOther:
+                         ox = int(nn.x * screen.get_width())
+                         oy = int(nn.y * screen.get_height())
+                         pygame.draw.line(screen, black, (newx, newy), (ox, oy), lineWidth)
                pygame.draw.circle(screen, aqua, (newx, newy), radius)
 
-run_game(400, 300, 60, RobotScene(50, 50))
+run_game(400, 300, 60, RobotScene(80, 80, 0.2))
--- rbutils.py
+++ rbutils.py
@@ -1,3 +1,19 @@
+import random
 
 def dist(r1, r2):
      return ((r1.x - r2.x)**2 + (r1.y - r2.y)**2)**(1.0/2)
+     
+def randomStep(rlist, xlo, xhi, ylo, yhi):
+     for rob in rlist:
+          dx = (2*random.random() - 1)
+          dy = (2*random.random() - 1)
+          dist = (dx**2 + dy**2)**(1.0/2)
+          dx /= dist
+          dy /= dist
+          rob.x += dx * rob.maxSpeed
+          rob.y += dy * rob.maxSpeed
+          
+          if rob.x < xlo: rob.x = xlo
+          if rob.x > xhi: rob.x = xhi
+          if rob.y < ylo: rob.y = ylo
+          if rob.y > yhi: rob.y = yhi
