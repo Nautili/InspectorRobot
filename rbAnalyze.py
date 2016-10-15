9b7d0b950852b86170aa9fe04743c5d12d36a692
nautili <lear.vg@gmail.com>
2016-10-15 15:54:32 -0500

    Commit before merging

--- rbAnalyze.py
+++ rbAnalyze.py
@@ -260,7 +260,44 @@ def generateCFGraphics(rootDir="Results\\MessagePath"):
         saveConfMat(fileName)
 
 
-def saveStatGraph(stats):
+def showExampleGraph():
+    contractData = (  0, 0.5, 0.1, 0.1, 0.2)
+    randomData   = (0.7, 0.3, 0.4, 0.3,   0)
+    resourceData = (  0, 0.1, 0.5, 0.6, 0.8)
+    disperseData = (0.3, 0.1,   0,   0,   0)
+
+    width = 1.0
+    ind = np.linspace(1,width*5,num=5)
+    colors = ("#ff7200", "#e80500", "#1917e8", "#cc00ff")
+
+    fig = plt.figure(figsize=(7,8))
+    globalFont = 16
+
+    p1 = plt.bar(ind, resourceData, width, color=colors[2], bottom=tuple(sum(t) for t in zip(randomData, disperseData, contractData)))
+    p2 = plt.bar(ind, contractData, width, color=colors[0], bottom=tuple(sum(t) for t in zip(randomData, disperseData)))
+    p3 = plt.bar(ind, disperseData, width, color=colors[1], bottom=randomData)
+    p4 = plt.bar(ind, randomData, width, color=colors[3])
+
+    plt.xlabel('Amount of Time (sec)')
+    plt.ylabel('Fraction of Predictions of Each Behavior', fontsize=globalFont)
+    plt.title("Accuracy for Random Motion vs. Time", fontsize=globalFont)
+    plt.xticks(ind + width/2., ("5-10", "10-15", "15-20", "20-25", "25-30"), fontsize=globalFont-2)
+    #plt.yticks(np.arange(0, 1, 0.1))
+    plt.tick_params(bottom="off",top="off")
+    plt.xlim([0.5,6.5*width])
+    plt.ylim([0,1.05])
+
+    #patches = [mpatches.Patch(color=col, label=key) for (col,key) in zip(colors,keys)]
+    #plt.legend(handles=patches, loc=7, bbox_to_anchor=(1.225, 0.5), fontsize=globalFont) #center right
+    plt.legend((p2[0], p3[0], p1[0], p4[0]), ("Contract", "Dispersal", "Resource Collection", "Random Motion"), loc=7, bbox_to_anchor=(1.55, 0.5))
+
+
+    #plt.show()
+    plt.savefig("Graphics\\4Graphlet\\All\\Comparisons\\StackExample.png", bbox_inches="tight")
+    plt.clf()
+
+
+def saveStatGraph(stats, title, fileName):
     keys = sorted(stats[0][0])
 
     predictedList = []
@@ -274,33 +311,42 @@ def saveStatGraph(stats):
     #plots = []
     width = 1.0
     ind = np.linspace(1,width*5,num=5)
-    colors = ["#ff9f01", "#dae820", "#0cff65", "#1187e8", "#9423ff"]
+    #colors = ["#ff9f01", "#dae820", "#0cff65", "#1187e8", "#9423ff"]
+    colors = ["#ff7200", "#e80500", "#cc00ff", "#1917e8", "#00bfff"]
     colorMap = dict(zip(keys, colors))
-    fig = plt.figure(figsize=(16,8))
-
+    fig = plt.figure(figsize=(24,8))
+    globalFont = 22
 
     for i in range(len(predictedList)):
         bottomHeights = [1.0] * 5
-        for key in keys:
+        for key in keys[i+1:] + keys[:i+1]:
             normalizedList = [val / 10.0 for val in predictedList[i][key]]
             curList = tuple(normalizedList)
             for j in range(len(curList)):
                 bottomHeights[j] -= curList[j]
             plt.bar(ind + 6*i*width, curList, width, color=colorMap[key], bottom=tuple(bottomHeights))
 
-
-    plt.ylabel('Fraction of Guesses')
-    plt.title('Varying Number of Blue Robots')
     xpositions = np.linspace(3.5*width,27.5*width,num=5)
-    plt.xticks(xpositions, tuple(keys))
+
+    plt.ylabel('Fraction of Predictions', fontsize=globalFont)
+    plt.title(title, fontsize=globalFont)
+    xpositions = np.linspace(3.5*width,27.5*width,num=5)
+    plt.xticks(xpositions, tuple(keys), fontsize=globalFont-2)
     #plt.yticks(np.arange(0, 1, 0.1))
+    plt.tick_params(bottom="off",top="off")
     plt.xlim([0,31*width])
     plt.ylim([0,1.05])
 
-    #patches = [mpatches.Patch(color=col, label=key) for (col,key) in zip(colors,keys)]
-    #plt.legend(handles=patches)
+    patches = [mpatches.Patch(color=col, label=key) for (col,key) in zip(colors,keys)]
+    plt.legend(handles=patches, loc=7, bbox_to_anchor=(1.225, 0.5), fontsize=globalFont) #center right
 
-    plt.show()
+    dirToSave = fileName.replace("Results", "Graphics")
+    fileName = os.path.basename(fileName)
+    dirToSave = dirToSave.replace(fileName, "Comparisons\\With Legends\\" + fileName).replace(".txt", ".png")
+
+    #plt.show()
+    plt.savefig(dirToSave, bbox_inches="tight")
+    plt.clf()
 
 
 def getVariableStatArray(resFile):
@@ -331,13 +377,78 @@ def getVariableStatArray(resFile):
     return statList
 
 
-def generateStackGraphs(rootDir="Results\\MessagePath\\All"):
-    fileList = ["BlueRobotsEP.txt"]
-    #fileList = ["BlueRobotsEP.txt", "RedRobotsEP.txt", "BlueVisionEP.txt", "RedVisionEP.txt", "TimeEP.txt"]
-    for fileName in fileList:
+def generateStackGraphs(rootDir="Results\\MessagePath\\All", preTitle = "Path Length: "):
+    #fileList = ["BlueRobotsEP.txt"]
+    fileList = ["BlueRobotsEP.txt", "RedRobotsEP.txt", "BlueVisionEP.txt", "RedVisionEP.txt", "TimeEP.txt"]
+    titleList = ["Varying Number of Observers (40 - 140)",
+                 "Varying Number of Agents (40 - 140)",
+                 "Varying Range of Observer Vision (0.15 - 0.25)",
+                 "Varying Range of Agent Vision (0.6 - 0.16)",
+                 "Varying Amount of Time (5 sec - 30 sec)"]
+    titleList = [preTitle + val for val in titleList]
+
+    argumentList = zip(fileList, titleList)
+    for fileName, title in argumentList:
         stats = getVariableStatArray(rootDir + "\\" + fileName)
         print("Generating stack graph for ", fileName)
-        saveStatGraph(stats)
+        saveStatGraph(stats, title, rootDir + "\\" + fileName)
+
+
+def generateLineGraphs():
+    #k-graphlet
+    gxVals = [1,5,10,25,50,100,500,1000,5000,10000,20000] #k-graphlet
+    gAcc = [0.33,0.55,0.60,0.66,0.71,0.73,0.79,0.81,0.84,0.84,0.86] #Accuracy
+    gSVM = [0.08,0.33,0.71,2.03,5.12,11.77,134.32,199.90,238.33,246.89,240.23]
+    gFeats = [0.98,1.22,1.57,2.55,4.17,7.40,33.26,65.88,331.97,655.03,1350.31]
+
+    mxVals = list(range(1,11)) #Message path
+    mAcc = [0.73,0.73,0.79,0.86,0.87,0.87,0.86,0.86,0.86,0.85]
+    mSVM = [17.88,2.59,0.38,0.15,0.21,0.14,0.25,0.29,0.18,0.18]
+    mFeats = [90.51,90.33,90.37,88.35,88.20,89.04,89.27,87.50,88.00,88.40]
+
+    plt.figure(1)
+
+    plt.subplot(231)
+    plt.plot(gxVals, gAcc)
+    plt.title("4-Graphlet Accuracy")
+    plt.xlabel("Number of Samples")
+    plt.ylim([0,1])
+    plt.grid(True)
+
+    plt.subplot(232)
+    plt.plot(gxVals, gFeats)
+    plt.title("4-Graphlet Time for Feature Vectors (sec)")
+    plt.xlabel("Number of Samples")
+    plt.grid(True)
+
+    plt.subplot(233)
+    plt.plot(gxVals, gSVM)
+    plt.title("4-Graphlet Training Time (sec)")
+    plt.xlabel("Number of Samples")
+    plt.grid(True)
+
+    plt.subplot(234)
+    plt.plot(mxVals, mAcc)
+    plt.title("Path Length Accuracy")
+    plt.xlabel("Max Path Length")
+    plt.ylim([0,1])
+    plt.grid(True)
+
+    plt.subplot(235)
+    plt.plot(mxVals, mFeats)
+    plt.title("Path Length Time for Feature Vectors(sec)")
+    plt.xlabel("Max Path Length")
+    plt.ylim([0,1400])
+    plt.grid(True)
+
+    plt.subplot(236)
+    plt.plot(mxVals, mSVM)
+    plt.title("Path Length Training Time (sec)")
+    plt.xlabel("Max Path Length")
+    plt.ylim([0,250])
+    plt.grid(True)
+
+    plt.show()
 
 
 def retrieveFeatures(dirToAnalyze="Data\\Pickles\\FeatureVectors\\MessagePath"):
@@ -376,6 +487,7 @@ def crossVal(dirToAnalyze="Data\\Pickles\\FeatureVectors\\4GraphletSmall", class
     exampleArray, labelArray = retrieveFeatures(dirToAnalyze)
     # Interleaved testing and training
 
+    print("Classifying...")
     clf = svm.SVC(kernel='linear',gamma=0.001)
     scores = cross_validation.cross_val_score(clf,exampleArray,labelArray,cv=10)
 
